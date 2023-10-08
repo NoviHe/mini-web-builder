f126f11acb807a285555d3645904fc63c8bb0585
NoviHe <claywhoami6@gmail.com>
2023-10-09 02:32:38 +0700

    update config

--- config/config.php
+++ config/config.php
@@ -1,16 +1,16 @@
 <?php
-define('DEVELOPMENT_ENVIRONMENT', true);
+define('DEVELOPMENT_ENVIRONMENT', false);
 // ===== Hosted ==================== //
 
-// define('BASE_PATH', 'https://live.nvireview.com');
-// define('URL_WEBSITE', 'https://live.nvireview.com');
-// define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/public');
+define('BASE_PATH', 'https://live.nvireview.com');
+define('URL_WEBSITE', 'https://live.nvireview.com');
+define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/public');
 
 // ===== Local ==================== //
 
-define('BASE_PATH', 'http://localhost/novi-builder');
-define('URL_WEBSITE', 'http://novi-builder.test');
-define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
+// define('BASE_PATH', 'http://localhost/novi-builder');
+// define('URL_WEBSITE', 'http://novi-builder.test');
+// define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
 // ================================//
 define('DEFAULT_CONTROLLER', 'home');
@@ -18,15 +18,15 @@ define('SITES_URL', $_SERVER['HTTP_HOST']);
 
 // ===== Hosted ==================== //
 
-// define('DB_NAME', 'noviherl_page_builder'); // Data base Name
-// define('DB_USER', 'noviherl_page_builder');
-// define('DB_PASSWORD', '50Q2c(c{(4kj');
+define('DB_NAME', 'noviherl_page_builder'); // Data base Name
+define('DB_USER', 'noviherl_page_builder');
+define('DB_PASSWORD', '50Q2c(c{(4kj');
 
 // ===== Local ==================== //
 
-define('DB_NAME', 'db_novi'); // Data base Name
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+// define('DB_NAME', 'db_novi'); // Data base Name
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', 'root');
 
 // ================================//
 define('DB_HOST', 'localhost');
